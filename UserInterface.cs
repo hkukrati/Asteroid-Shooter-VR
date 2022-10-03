badf302147d72da4ad883a57fba55f0dacd2f742
Harshi Kukrati <harshikukrati@gmail.com>
2022-10-03 16:55:58 -0700

    Add user interface script

---
+++ UserInterface.cs
@@ -0,0 +1,21 @@
+// Script that's tied to the Start Game Button within the Start Menu canvas
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UserInterface : MonoBehaviour
+{
+    public UnityEvent onShotByGun;
+
+    // countDownCanvas holds the "3, 2, 1" animation countdown
+    [SerializeField] private Canvas countDownCanvas;
+    
+    // Start is called before the first frame update
+    public void shotByGun()
+    {
+        onShotByGun.Invoke();
+        Canvas newCanvas = Instantiate(countDownCanvas, new Vector3(2766, 560, 3405), Quaternion.identity);
+    }
+}
