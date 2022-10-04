f3d3d61fdf8d2a9a5f2e3375b055990cdf2fcb74
Harshi Kukrati <harshikukrati@gmail.com>
2022-10-03 17:08:38 -0700

    Update UserInterface.cs comments

--- UserInterface.cs
+++ UserInterface.cs
@@ -12,7 +12,14 @@ public class UserInterface : MonoBehaviour
     // countDownCanvas holds the "3, 2, 1" animation countdown
     [SerializeField] private Canvas countDownCanvas;
     
-    // Start is called before the first frame update
+/* 
+ * shotByGun() is called in the Laser Gun script upon a successful Raycast hit event with the "Start Game" button
+ * A UnityEvent (onShotByGun) is tied to this function. From configuration in the Inspector, invoking this UnityEvent performs the following actions:
+ * - Hides the Start Menu canvas
+ * - Hides the Game Over and Score text 
+ * - Calls startGame() from the GameController to begin the game loop 
+ * The Canvas prefab that holds the "3, 2, 1" animation is also instantiated in the same position as the Start Menu canvas 
+ */ 
     public void shotByGun()
     {
         onShotByGun.Invoke();
